2d10e40cb367b9283212741776b8476b93f72d1e
Nadiya <diyay258@gmail.com>
2024-12-09 16:09:23 +0700

    Upload file started

--- app/src/main/java/id/ac/polbeng/nadiya/githubprofile/helpers/Config.kt
+++ app/src/main/java/id/ac/polbeng/nadiya/githubprofile/helpers/Config.kt
@@ -1,7 +1,7 @@
 package id.ac.polbeng.nadiya.githubprofile.helpers
 
 import de.hdodenhof.circleimageview.BuildConfig
-
+//nadiyanur
 class Config {
     companion object{
         const val SPLASH_SCREEN_DELAY:Long = 3000
